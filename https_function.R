0e3b9e8a096bbc37bb726ced0194d0e30799702d
Rich Heimann <heimann.richard@gmail.com>
2013-10-19 21:04:59 -0400

    https_function
    https_function

---
+++ https_function.R
@@ -0,0 +1,11 @@
+https_function <- function(url, ...) {
+  # load package
+  require(RCurl)
+  
+  # parse and evaluate each .R script
+  sapply(c(url, ...), function(u) {
+    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
+  })
+}
+
+https_function("https://raw.github.com/rheimann/sentiment_function/master/sentiment.R")
